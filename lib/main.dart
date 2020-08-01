b9dbc1cc3a6d4e23aeebf37fe314bf5d2ce764fc
Neeraj <palliyali.neeraj@gmail.com>
2020-08-01 23:36:40 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:ShoppingApp/screens/products_overview_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}

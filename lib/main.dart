b048a618a44c606a0264dc961ff85b7521612b26
Neeraj <palliyali.neeraj@gmail.com>
2020-11-23 22:04:11 +0530

    User Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ShoppingApp/providers/auth.dart';
 import 'package:ShoppingApp/providers/cart.dart';
 import 'package:ShoppingApp/providers/orders.dart';
 import 'providers/products.dart';
@@ -7,6 +8,7 @@ import 'package:ShoppingApp/screens/products_overview_screen.dart';
 import 'package:ShoppingApp/screens/edit_product_screen.dart';
 
 import 'screens/user_products_screen.dart';
+import 'screens/auth_screen.dart';
 import 'screens/cart_screen.dart';
 import 'screens/orders_screen.dart';
 
@@ -22,34 +24,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (ctx) => Products(),
-          ),
-          ChangeNotifierProvider(
-            create: (ctx) => Cart(),
-          ),
-          ChangeNotifierProvider(
-            create: (ctx) => Orders(),
-          ),
-        ],
-        child: MaterialApp(
-          title: 'Shopping App',
-          theme: ThemeData(
-            primarySwatch: Colors.purple,
-            accentColor: Colors.deepOrange,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            fontFamily: 'Lato',
-          ),
-          home: ProductsOverviewScreen(),
-          debugShowCheckedModeBanner: false,
-          routes: {
-            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-            CartScreen.routeName: (ctx) => CartScreen(),
-            OrdersScreen.routeName: (ctx) => OrdersScreen(),
-            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-            EditProductScreen.routeName: (ctx) => EditProductScreen(),
-          },
-        ));
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Shopping App',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Lato',
+        ),
+        home: AuthScreen(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
+        },
+      ),
+    );
   }
 }
---
+++ lib/providers/auth.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _exipryDate;
+  String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCmRmr7hz8HhvU21eQcMywj_XckStWg5oM';
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+}
